ad6b39544b9f483af95798514b5e2c183bcfac6d
Joe Sadusk <joe@sadusk.com>
2022-04-15 16:07:52 -0700

    Try out ignoring multi column and row presses

--- matrix_keypad/RPi_GPIO.py
+++ matrix_keypad/RPi_GPIO.py
@@ -37,7 +37,9 @@ class keypad():
         for j in range(len(self.COLUMN)):
             GPIO.setup(self.COLUMN[j], GPIO.OUT)
             GPIO.output(self.COLUMN[j], GPIO.LOW)
-         
+
+        time.sleep(0.001)
+        
         # Set all rows as input
         for i in range(len(self.ROW)):
             GPIO.setup(self.ROW[i], GPIO.IN, pull_up_down=GPIO.PUD_UP)
@@ -48,7 +50,11 @@ class keypad():
         for i in range(len(self.ROW)):
             tmpRead = GPIO.input(self.ROW[i])
             if tmpRead == 0:
-                rowVal = i
+                if rowVal == -1:
+                    rowVal = i
+                else:
+                    self.exit()
+                    return
                  
         # if rowVal is not 0 thru 3 then no button was pressed and we can exit
         if rowVal <0 or rowVal >3:
@@ -69,7 +75,11 @@ class keypad():
         for j in range(len(self.COLUMN)):
             tmpRead = GPIO.input(self.COLUMN[j])
             if tmpRead == 1:
-                colVal=j
+                if colVal == -1:
+                    colVal = j
+                else:
+                    self.exit()
+                    return
                  
         # if colVal is not 0 thru 2 then no button was pressed and we can exit
         if colVal <0 or colVal >2:
